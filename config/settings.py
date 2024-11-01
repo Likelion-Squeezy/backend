93aa5995d12ff1c78dd5d4e642bfea1da394e456
mongsam2 <songjaehyun121476@gmail.com>
2024-11-01 15:58:43 +0900

    feat: debug true

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t_dv&(qls7_fl@=)#(%qcv34y7y1=l-^ipelf*$^+9183=s5x&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
