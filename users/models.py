d5260bd48914e88ad28f23af0fba2f08e55b329d
mongsam2 <songjaehyun121476@gmail.com>
2024-11-06 10:27:26 +0900

    feat: 유저 이메일 중복 불가 및 서비스 사용 제한 횟수

--- users/models.py
+++ users/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), blank=True, unique=True)
+    eezy_count = models.IntegerField(default=10)
+    squeezy_count = models.IntegerField(default=5)
