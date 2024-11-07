a5ca8619a5708eb22eb925933a477fed170039f0
mongsam2 <songjaehyun121476@gmail.com>
2024-11-07 18:40:18 +0900

    feat: 로컬 테스트 환경에 맞게 settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t_dv&(qls7_fl@=)#(%qcv34y7y1=l-^ipelf*$^+9183=s5x&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['13.124.143.64']
+ALLOWED_HOSTS = ['13.124.143.64', '127.0.0.1']
 
 # Application definition
 
