da633e4a63b2d23148d5f2bb2f721c9da8cac505
dltnals1210 <dltnalstnals14@gmail.com>
2024-11-07 21:12:02 +0900

    feat: get-eezy 기능 추가
    - eezy app 에서만 수정
    - admin.py -> list_display에서 tab__user 삭제
        - migrate 시 tab__user 를 불러오는 과정에서 계속해서 오류가 발생하여 삭제
        - 어차피 tab을 불러오면 그 안에서 수정하므로 문제없는 것 같습니다
    - serializers.py -> tab = TabSerializer(read_only=True) 의 위치 수정
        - 이전엔 Meta 클래스 안의 맨 아래 있었으나, Meta 클래스 외부 맨 위로 위치 수정
        - migrate 시에 tab을 불러올 때 계속해서 오류가 나, 이의 해결을 위해 수정했
    - tests.py -> get-eezy 기능 테스트를 위한 메소드 2개 추가
    - urls.py -> eezy_id로 get 해오기 위한 url 패턴 추가
    - views.py -> get method 추가

--- eezy/tests.py
+++ eezy/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from tabs.models import Tab
+from eezy.models import Eezy
+from users.models import User
+
+class EezyViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.tab = Tab.objects.create(user=self.user, title="UX란?", url="www.com")
+        self.eezy = Eezy.objects.create(title="UX UI 기초", content="대충 페이지 요약한 내용.....", tab=self.tab)
+
+    def test_get_eezy(self):
+        url = reverse('eezy-detail', args=[self.eezy.id])
+        response = self.client.get(url)
+
+        expected_data = {
+            "id": self.eezy.id,
+            "title": self.eezy.title,
+            "content": self.eezy.content,
+            "tab": {
+                "id": self.tab.id,
+                "title": self.tab.title,
+                "url": self.tab.url
+            }
+        }
+        self.assertEqual(response.json(), expected_data)
