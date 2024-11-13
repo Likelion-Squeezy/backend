1d521cb0cbc88d04db6f4c03f2b6b129151c1158
dltnals1210 <dltnalstnals14@gmail.com>
2024-11-13 17:52:42 +0900

    feat: 유저 프로필 조회 response 수정
    - eezy, squeeze 모델에 created_at 필드 추가
    - 수정된 response body에 따라 user profile serializer, UserView get api 수정

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,35 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from squeeze.serializers import SqueezeSerializer
-from eezy.serializers import EezySerializer
-
-User = get_user_model()
+from .models import User
+from squeeze.models import Squeeze
+from eezy.models import Eezy
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    squeeze = SqueezeSerializer(many=True)
-    eezy = EezySerializer(many=True)
+    history = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'squeeze', 'eezy']
+        fields = ['id', 'username', 'email', 'history']
+
+    def get_history(self, obj):
+        squeeze_qs = Squeeze.objects.filter(user=obj).order_by('-created_at')
+        eezy_qs = Eezy.objects.filter(user=obj).order_by('-created_at')
+
+        history = [
+            {
+                "type": "squeezy",
+                "id": squeeze.id,
+                "title": squeeze.title,
+                "created_at": squeeze.created_at,
+            }
+            for squeeze in squeeze_qs
+        ] + [
+            {
+                "type": "eezy",
+                "id": eezy.id,
+                "title": eezy.title,
+                "created_at": eezy.created_at,
+            }
+            for eezy in eezy_qs
+        ]
+
+        return sorted(history, key=lambda x: x['created_at'], reverse=True)
--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotAuthenticated
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
 from squeeze.models import Squeeze
 from eezy.models import Eezy
-from squeeze.serializers import SqueezeSerializer
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from .serializers import UserProfileSerializer
+
+
 
 class UserView(APIView):
     permission_classes = [IsAuthenticated]
@@ -23,34 +23,6 @@ class UserView(APIView):
         if user.is_anonymous:
             raise NotAuthenticated("자격 인증이 필요합니다.")
 
-        # Squeeze 정보 가져오기
-        squeeze_qs = Squeeze.objects.filter(user=user).prefetch_related('tabs')
-        squeeze_data = [
-            {
-                "id": squeeze.id,
-                "title": squeeze.title,
-                "count": squeeze.tabs.count(),
-            }
-            for squeeze in squeeze_qs
-        ]
-
-        # Eezy 정보 가져오기
-        eezy_qs = Eezy.objects.filter(user=user).select_related('tab')
-        eezy_data = [
-            {
-                "id": eezy.id,
-                "title": eezy.title,
-            }
-            for eezy in eezy_qs
-        ]
-
-        # 사용자 정보와 함께 응답 생성
-        response_data = {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-            "squeeze": squeeze_data,
-            "eezy": eezy_data,
-        }
-
-        return Response(response_data, status=status.HTTP_200_OK)
+        # 사용자 정보와 함께 응답 생성 (시리얼라이저 사용)
+        user_serializer = UserProfileSerializer(user)
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
